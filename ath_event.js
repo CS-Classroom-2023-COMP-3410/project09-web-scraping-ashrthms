99f3b244b64a3fe1cdaa24be9581297452e5c829
Asher <ashermthomas@gmail.com>
2026-03-13 03:00:50 +0000

    ath data found, comp transfer

--- ath_event.js
+++ ath_event.js
@@ -4,7 +4,7 @@ const { default: fsExtra } = require('fs-extra/esm');
 const fs = require('fs');
 
 
-axios.get('https://denverpioneers.com/site/templates/slideshow-component-template.html')
+axios.get('https://denverpioneers.com/')
     .then(response => {
         const $ = cheerio.load(response.data);
         
@@ -12,38 +12,43 @@ axios.get('https://denverpioneers.com/site/templates/slideshow-component-templat
         let dataScript = null;
         $('script').each((i, script) => {
             const html = $(script).html();
-            if (html && html.includes('"type":"events"')) {
-                dataScript = html;
+            if (html && html.includes('"school_name":"University of Denver"') && html.includes('var obj = ')) {
+                console.log(html.indexOf('"'), html.indexOf(";"))
+                const obj = JSON.parse(JSON.stringify(html.slice(html.indexOf('{'),html.indexOf(';')-1)))
+                // dataScript = html;
+                obj.filter()
             }
         });
+
         
-        if (!dataScript) {
-            console.log('Event data not found');
-            return;
-        }
         
-        // Step 2: Extract the JSON from "var obj = {...}"
-        const jsonMatch = dataScript.match(/var obj = ({[\s\S]*?});/);
-        if (!jsonMatch) {
-            console.log('Could not parse JSON');
-            return;
-        }
+        // if (!dataScript) {
+        //     console.log('Event data not found');
+        //     return;
+        // }
         
-        // Step 3: Parse the JSON string into a JavaScript object
-        const obj = JSON.parse(jsonMatch[1]);
+        // // Step 2: Extract the JSON from "var obj = {...}"
+        // const jsonMatch = dataScript.match(/var obj = ({[\s\S]*?});/);
+        // if (!jsonMatch) {
+        //     console.log('Could not parse JSON');
+        //     return;
+        // }
         
-        // Step 4: Use the data
-        const formatted = {'events': []};
-        obj.data.forEach(event => {
-            formatted.events.push({
-                duTeam: event.sport,
-                opponent: event.opponent_id,
-                date: event.date
-            });
-        });
+        // // Step 3: Parse the JSON string into a JavaScript object
+        // const obj = JSON.parse(jsonMatch[1]);
         
-        fs.writeFileSync('results/athletic_events.json', JSON.stringify(formatted, null, 2));
-        console.log(`Saved ${formatted.events.length} events`);
+        // // Step 4: Use the data
+        // const formatted = {'events': []};
+        // obj.data.forEach(event => {
+        //     formatted.events.push({
+        //         duTeam: event.sport,
+        //         opponent: event.opponent_id,
+        //         date: event.date
+        //     });
+        // });
+        
+        // fs.writeFileSync('results/athletic_events.json', JSON.stringify(formatted, null, 2));
+        // console.log(`Saved ${formatted.events.length} events`);
 
     })
     .catch(error =>
